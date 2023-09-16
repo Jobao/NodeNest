544f9abd734ebf19fa0ee7ae0cc376f042d66db2
Joao Lucas Ramos Suarez <ramossuarezjl@gmail.com>
2023-09-16 09:51:38 -0300

    modificado para usar herencia

--- APIS/inventor-api/src/dto/Category/get-Category.dto.ts
+++ APIS/inventor-api/src/dto/Category/get-Category.dto.ts
@@ -1,10 +1,8 @@
 import { IsNotEmpty} from "class-validator";
+import { CategoryDto } from "./category.dto";
 
-export class GetCategoryDto {
+export class GetCategoryDto extends CategoryDto {
 
     @IsNotEmpty()
     id: number;
-
-    @IsNotEmpty()
-    desc: string;
 }
